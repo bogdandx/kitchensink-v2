37bb7840b101b1b954c11c47433501d6937e78bd
bogdan <bogdandumitrescu@yahoo.com>
2024-10-07 19:24:34 +0300

    Configured Maven to also run Cucumber tests.

---
+++ src/test/java/KitchenSink/RunCucumberTest.java
@@ -0,0 +1,13 @@
+package KitchenSink;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@EnabledOnOs({ OS.WINDOWS })
+@CucumberOptions(features = "src/test/resources", glue = "steps")
+public class RunCucumberTest {
+}
